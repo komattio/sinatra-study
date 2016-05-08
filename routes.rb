62985d226eca3458c3c960247cc039ebbf826d64
komattio <y.komatsu9@gmail.com>
2016-05-08 12:30:47 +0900

    regexp sample

--- routes.rb
+++ routes.rb
@@ -19,7 +19,13 @@ get '/download/*.*' do |path, extl|
     [path]
 end
 
+# "?"を使うと空白でも許容される
 get '/hello/:fname/?:lname?' do |f, l|
     "hello #{f} #{l}."
+end
+
+# 正規表現の使用
+get %r{/users/([0-9]*)} do |id|
+    "user id = #{id}."
 end
 
